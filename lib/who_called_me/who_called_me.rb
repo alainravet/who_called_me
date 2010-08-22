5268337fc0517beb0e6f4cfe01b4e16edb217931
Alain Ravet <alain.ravet+git@gmail.com>
2010-08-23 00:13:45 +0200

    refactoring : store info in a hash instead of array, to prepare for calls with a key/msg.

--- lib/who_called_me/who_called_me.rb
+++ lib/who_called_me/who_called_me.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + '/trace_utils'
 
 module WhoCalledMe
 
-  @@__who_called_me_accumul = []
+  @@__who_called_me_accumul_h = {}
   @@__disable_puts          = false
 
   def self.disable_puts ; @@__disable_puts = true  end
@@ -17,14 +17,20 @@ module WhoCalledMe
     trace = trace[nof_internal_methods_to_skim..-1]
     trace.reverse!
 
-    _who_called_me_data << trace
+    record(msg || :all, trace)
 
     trace
   end
 
+  def self.record(key, trace)
+    @@__who_called_me_accumul_h[key] ||= []
+    @@__who_called_me_accumul_h[key] << trace
+  end
+
+
 
   def self._who_called_me_data(options={})
-    all = @@__who_called_me_accumul
+    all = @@__who_called_me_accumul_h[:all]
     if options[:only_top]
       all.collect{|trace| trace.first }.uniq
     else
@@ -40,14 +46,16 @@ module WhoCalledMe
     puts l0
     puts 'who_called_me report :'
     puts '======================'
-    _who_called_me_data.each do |trace|
-      puts
-      puts trace.shift
-      puts '  was called by :'
-      padding = ''
-      trace.each do |line|
-        padding += '  '
-        puts "  .#{padding}#{line}"
+    _who_called_me_data.keys.each do |key|
+      _who_called_me_data[key].each do |trace|
+        puts
+        puts trace.shift
+        puts '  was called by :'
+        padding = ''
+        trace.each do |line|
+          padding += '  '
+          puts "  .#{padding}#{line}"
+        end
       end
     end
     puts
